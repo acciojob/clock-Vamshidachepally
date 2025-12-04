53ab61c36acb279a5cd33b92731c0c3471728262
Vamshidachepally <varmavamshi282@gmail.com>
2025-12-04 19:13:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+//your JS code here. If required.
+setInterval(()=>{
+            let today=new Date()
+            let date=today.toLocaleDateString()
+            let time=today.toLocaleTimeString()
+            document.getElementById("timer").innerText=`${date} ${time}`
+
+        },1000)
